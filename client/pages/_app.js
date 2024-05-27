3fca78c03cd3fb3e2bf462b844f44168c5ce7541
Apollos David <apollos.david@adaire.com>
2024-05-27 12:32:17 +0200

    Get init props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
 
-
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return  <Component {...pageProps} />
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+   pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+
+  }
+
+  return {
+    pageProps,
+    ...data
+  }
+}
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import buildClient from "../api/build-client";
+const index = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
 
-const index = () => {
-  return (
-    <h1>Landing page2</h1>
-  )
-}
+index.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
 
-export default index
+  return data;
+};
+
+export default index;
