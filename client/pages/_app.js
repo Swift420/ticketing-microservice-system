4e19ad8bd778a40841be18b929d4c19ae474e28a
Apollos David <apollos.david@adaire.com>
2024-05-28 13:59:29 +0200

    added common

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,26 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import buildClient from '../api/build-client';
-
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
-  return  <Component {...pageProps} />
-}
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
 
 AppComponent.getInitialProps = async (appContext) => {
-
   const client = buildClient(appContext.ctx);
-  const { data } = await client.get('/api/users/currentuser');
+  const { data } = await client.get("/api/users/currentuser");
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-   pageProps = await appContext.Component.getInitialProps(appContext.ctx);
-
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
   }
 
   return {
     pageProps,
-    ...data
-  }
-}
+    ...data,
+  };
+};
 
 export default AppComponent;
